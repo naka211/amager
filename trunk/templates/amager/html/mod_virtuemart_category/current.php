88a607c3b9e574767fdf76cb9440a2bf398646f2
dreamwatcher2007 <dreamwatcher2007@cf09e72b-acc9-d41d-0836-3fd438f12a20>
2013-01-23 10:27:43 +0000

    final version of VM categories module

--- trunk/templates/amager/html/mod_virtuemart_category/current.php
+++ trunk/templates/amager/html/mod_virtuemart_category/current.php
@@ -1,28 +1,30 @@
 <?php // no direct access
 defined('_JEXEC') or die('Restricted access');
-//JHTML::stylesheet ( 'menucss.css', 'modules/mod_virtuemart_category/css/', false );
+
 $ID = str_replace('.', '_', substr(microtime(true), -8, 8));
-//echo '<pre>',print_r($categories),'</pre>';die;
 function subrender($categories, $level=1, $chaindata=array()){
 	?>
 	<ul class="<?php echo $chaindata[0].$level; ?>">
 	<?php
 		foreach ($categories as $category){
+			$parent_mark='';
 			$caturl = JRoute::_('index.php?option=com_virtuemart&view=category&virtuemart_category_id='.$category->virtuemart_category_id);
 			$cattext = $category->category_name;
 
-			if($chaindata[3]==$category->virtuemart_category_id):?>
-				<li class="active-sub">
-				<?php
+			if($chaindata[3]==$category->virtuemart_category_id):
+				echo '<li class="active-sub'.$level.'">';
 			else:
-				echo '<li>';
+				if(in_array( $category->virtuemart_category_id, $chaindata[4]))
+					$parent_mark = ' class="active-sub'.$level.'"';
+				?>
+				<li<?php echo $parent_mark?>>
+				<?php
 			endif;
 
 			echo JHTML::link($caturl, $cattext);
 			$category->childs = $chaindata[1]->call( array( 'VirtueMartModelCategory', 'getChildCategoryList' ),$chaindata[2], $category->virtuemart_category_id );
-			if(!empty($category->childs)){
+			if($category->childs)
 				subrender($category->childs, $level+1, $chaindata);
-			}
 				?>
 			</li>
 			<?php
@@ -35,26 +37,20 @@ function subrender($categories, $level=1, $chaindata=array()){
 <div class="cate">
 <ul class="VMmenu<?php echo $class_sfx ?>" id="<?php echo "VMmenu".$ID ?>" >
 <?php foreach ($categories as $category) {
-		 $active_menu = 'class="VmClose"';
+		$active_menu = ' class="VmClose"';
 		$caturl = JRoute::_('index.php?option=com_virtuemart&view=category&virtuemart_category_id='.$category->virtuemart_category_id);
 		$cattext = $category->category_name;
-		//if ($active_category_id == $category->virtuemart_category_id) $active_menu = 'class="active"';
-		if (in_array( $category->virtuemart_category_id, $parentCategories)) $active_menu = 'class="VmOpen"';
-
+		if (in_array( $category->virtuemart_category_id, $parentCategories))
+			$active_menu = ' class="active-cate"';
 		?>
-
-<li <?php echo $active_menu ?>>
-		<?php echo JHTML::link($caturl, $cattext);
-		if ($category->childs) {
-
-		}
-		?>
-<?php if ($active_menu=='class="VmOpen"') {
-	subrender($category->childs,1,array($class_sfx, $cache, $vendorId, $categoryModel->_id));
-}
+<li<?php echo $active_menu ?>>
+		<?php
+		echo JHTML::link($caturl, $cattext);
+		if (strpos($active_menu, 'active'))
+			subrender($category->childs,1,array($class_sfx, $cache, $vendorId, $categoryModel->_id, $parentCategories));
 ?>
 </li>
 <?php
-	} ?>
+} ?>
 </ul>
 </div>
